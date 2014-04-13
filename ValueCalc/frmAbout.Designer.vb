52a31bcaf74fc7e3be18252836416d228f0b4126
NimitzDEV <nimitzdevteam@gmail.com>
2014-04-13 18:58:34 +0800

    更改关于界面，清除WMI引用

--- ValueCalc/frmAbout.Designer.vb
+++ ValueCalc/frmAbout.Designer.vb
@@ -29,6 +29,8 @@ Partial Class frmAbout
         Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'LinkLabel10
@@ -85,7 +87,7 @@ Partial Class frmAbout
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("微软雅黑", 42.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.ForeColor = System.Drawing.Color.DodgerBlue
         Me.Label1.Location = New System.Drawing.Point(44, 26)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(336, 75)
@@ -102,12 +104,34 @@ Partial Class frmAbout
         Me.LinkLabel1.TabStop = True
         Me.LinkLabel1.Text = "源代码"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DarkOrange
+        Me.Label2.Location = New System.Drawing.Point(55, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 16)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Powered by "
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(353, 136)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "关闭"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'frmAbout
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(430, 162)
         Me.ControlBox = False
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.LinkLabel10)
@@ -130,4 +154,6 @@ Partial Class frmAbout
     Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
--- ValueCalc/frmAbout.vb
+++ ValueCalc/frmAbout.vb
@@ -1,15 +1,5 @@
-﻿Imports System.Runtime.InteropServices
+﻿
 Public Class frmAbout
-    <StructLayout(LayoutKind.Sequential)> _
-    Public Structure MARGINS
-        Public cxLeftWidth As Integer
-        Public cxRightWidth As Integer
-        Public cyTopHeight As Integer
-        Public cyButtomheight As Integer
-    End Structure
-    <DllImport("dwmapi.dll")> _
-    Public Shared Function DwmExtendFrameIntoClientArea(ByVal hWnd As IntPtr, ByRef pMarinset As MARGINS) As Integer
-    End Function
     Private Sub LinkLabel2_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
         Process.Start("http://weibo.com/NimitzDEV")
     End Sub
@@ -30,27 +20,11 @@ Public Class frmAbout
         Process.Start("https://me.alipay.com/nimitzdev")
     End Sub
 
-    Private Sub frmAbout_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Click
-        Me.Close()
-    End Sub
-
-    Private Sub frmAbout_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'Win7/Vista 毛玻璃效果
-        'Win8 Metro 土鳖不上档次的平面效果
-        On Error Resume Next
-        Me.BackColor = Color.Black
-        Dim margins As MARGINS = New MARGINS
-        margins.cxLeftWidth = -1
-        margins.cxRightWidth = -1
-        margins.cyTopHeight = -1
-        margins.cyButtomheight = -1
-        '这四个值分别是透明区域的左、右、上、下的高度，如果设置为-1则说明全透明。
-        '如果设置指定区域则输入0以上的数字
-        Dim hwnd As IntPtr = Me.Handle
-        Dim result As Integer = DwmExtendFrameIntoClientArea(hwnd, margins)
-    End Sub
-
     Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         Process.Start("https://github.com/nimitzdev/calc4lounger")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
     End Sub
 End Class
