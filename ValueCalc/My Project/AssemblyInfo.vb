0cd4e14158198d463f6d7895b0d3cd2df4196788
NimitzDEV <nimitzdevteam@gmail.com>
2014-04-20 18:59:39 +0800

    0.5.8.90

--- ValueCalc/My Project/AssemblyInfo.vb
+++ ValueCalc/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.5.7.84")> 
-<Assembly: AssemblyFileVersion("0.5.7.84")> 
+<Assembly: AssemblyVersion("0.5.8.90")> 
+<Assembly: AssemblyFileVersion("0.5.8.90")> 
